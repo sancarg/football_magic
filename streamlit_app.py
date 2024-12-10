69e43c22449ad6c56941774fbe2489438e72648b
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-10 11:15:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 st.title('⚽ Football Statistics App')
 # Load the dataset from the URL
 url = "https://www.kaggle.com/datasets/davidcariboo/player-scores/download/player_valuations.csv"
-df = pd.read_csv(url)
-
+html = pd.read_html(url, header =1)
+df = html[0]
 
 
