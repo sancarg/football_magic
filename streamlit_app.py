c3af71453715c357ebfa83efbbbeb86d71dc4264
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-10 10:47:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# Ensure Kaggle credentials are set up 
-os.environ['KAGGLE_CONFIG_DIR'] = "/root/.kaggle"
-
-# Download the dataset from Kaggle 
-kaggle datasets download -d davidcariboo/player-scores
 
 st.title('⚽ Football Statistics App')
+# Load the dataset from the URL
+url = "https://www.kaggle.com/datasets/davidcariboo/player-scores/download/player_valuations.csv"
+df = pd.read_csv(url)
 
 
 
