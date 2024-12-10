0fe77c8472d62995da7be6230752e46b15fe8db3
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-10 11:33:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import os
 
 st.title('⚽ Football Statistics App')
 # Load the dataset from the URL
+# Get Kaggle credentials from Streamlit Secrets
+os.environ['GSancar'] = st.secrets["credentials"]["GSancar"]
 #url = "https://www.kaggle.com/datasets/davidcariboo/player-scores/download/player_valuations.csv"
 
 
