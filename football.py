e27366ee3c5447fdc4442214a73f10197caca270
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-10 21:12:23 +0300

    Update football.py

--- football.py
+++ football.py
@@ -1,1 +1,14 @@
+from cProfile import label
+from typing import List
+import streamlit as st
+import os
+from pathlib import Path
+import pandas as pd
+from inflection import dasherize, titleize
+from datetime import datetime, timedelta
 
+import base64
+
+import sys
+cwd = os.getcwd()
+sys.path.insert(0, cwd)
