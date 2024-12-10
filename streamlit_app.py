4ab042a43f69cd087bc230f28521b3f9c99ca52f
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-10 10:41:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import altair as alt
 import os
+# Ensure Kaggle credentials are set up 
+os.environ['KAGGLE_CONFIG_DIR'] = "/root/.kaggle"
+
 st.title('⚽ Football Statistics App')
 
 
