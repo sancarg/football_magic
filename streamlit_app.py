8d3182a98cc871f947b4fe4c79db045659d5d692
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-10 12:28:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,11 @@ import numpy as np
 import pandas as pd
 import os
 
+# Set the page configuration 
+st.set_page_config(page_title="Movie Insights Analyzer", page_icon="📽", layout="wide")
+
 st.title('⚽ Football Statistics App')
 # Load the dataset from the URL 
-df = pd.read_csv('https://github.com/abineshta/FIFA-22-complete-player-dataset-EDA/blob/main/players_22.csv')
+#df = pd.read_csv('https://github.com/abineshta/FIFA-22-complete-player-dataset-EDA/blob/main/players_22.csv')
 
 
